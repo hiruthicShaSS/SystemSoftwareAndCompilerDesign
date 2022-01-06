3d8562aaa00915ae84d6dee61ab80cc080f7e6ba
hiruthicShaSS <predatorsah2002@gmail.com>
2022-01-06 18:44:36 +0530

    return_type changing with the datatype error fixed

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,39 +1,45 @@
 import re
+import copy
 
 tokens = {
     "keywords": ["int", "float", "double", "char", "string" "class", "struct"],
     "operators": ["=", "-", "+", "*", "/"],
     "relational": ["<=", ">=", "==", "!="],
-    "punctuation": [",", ";"]
+    "punctuation": [",", ";"],
 }
 
 FUNCTION_EXPRESSION = re.compile(r"\w{1,}\(.*\)")
-MAGIC_EXPRESSION = re.compile(r"[a-zA-Z_$0-9]{1,}")
+MAGICAL_EXPRESSION = re.compile(r"[a-zA-Z_$0-9]{1,}")
 
 
 class Symbol:
     def __init__(self) -> None:
         self.id = None
         self.data_type = None
-        self.return_type = None
         self.initial_value = 0
-        self.num_params = None
-        self.type_of_params = None
         self.isFunction = False
-        self.functionParams = None
+        self.return_type = None
+        self.function_params = None
+        self.type_of_params = None
+        self.num_params = None
 
-    # def __eq__(self, __o: object) -> bool:
-    #     return self.id == __o.id
+    def __eq__(self, __o: object) -> bool:
+        return self.id == __o.id
 
-    # def __hash__(self) -> int:
-    #     return hash(("id", self.id))
+    def __hash__(self) -> int:
+        return hash(("id", self.id))
 
     def __str__(self) -> str:
         return f"{self.id}\t{self.data_type}\t\t{self.return_type}\t\t{self.initial_value}\t\t{self.num_params}\t\t\t{self.type_of_params}"
 
 
-def charInTokenDB(char: str) -> bool:
-    return char.strip() in tokens["operators"] or char in tokens["relational"] or char in tokens["punctuation"]
+def charInTokenDB(char: str):
+    char = char.strip()
+    return (
+        char in tokens["operators"]
+        or char in tokens["punctuation"]
+        or char in tokens["relational"]
+    )
 
 
 symbol_table = []
@@ -61,7 +67,7 @@ def tryCast(value, expectedType):
 
 with open("expression.txt") as file:
     for line in file.readlines():
-        all_tokens = MAGIC_EXPRESSION.findall(line)
+        all_tokens = MAGICAL_EXPRESSION.findall(line)
 
         lastDataType = ""
         symbol = Symbol()
@@ -71,12 +77,9 @@ with open("expression.txt") as file:
                 symbol.data_type = token
                 lastDataType = token
 
-                # Verify if initial value matches the declared datatype
                 try:
-                    if (
-                        lastDataType == "int"
-                        and tryCast(all_tokens[index + 1], lastDataType)
-                        != None
+                    if lastDataType == "int" and tryCast(
+                        all_tokens[index + 1], lastDataType
                     ):
                         symbol.initial_value = all_tokens[-1]
                     elif (
@@ -96,42 +99,39 @@ with open("expression.txt") as file:
                         symbol.initial_value = all_tokens[-1]
                 except ValueError as e:
                     pass
-                # Datatype verification end ==============================
             else:
                 symbol.id = all_tokens[1]
-                # print(all_tokens[index])
 
             if len(FUNCTION_EXPRESSION.findall(line)) > 0:
                 symbol.isFunction = True
-                params = [
-                    word for word in all_tokens if word in tokens["keywords"]]
-
-                # -1 for compensating return type
+                params = [word for word in all_tokens if word in tokens["keywords"]]
                 symbol.num_params = len(params) - 1
-                symbol.type_of_params = params[:-1]
                 symbol.initial_value = None
+                symbol.type_of_params = ", ".join(params[:-1])
                 symbol.return_type = symbol.data_type
-                # symbol.data_type = None  # Uncommenting this line updates return_type for some reasons, uncomment and get wrkt
+                symbol1 = copy.deepcopy(symbol)  # Something wired happens here!
+                symbol1.data_type = None
+                symbol_table.append(symbol1)
+                continue
 
-            # symbol.initial_value = all_tokens[-1]
             symbol_table.append(symbol)
 
 
-symbol_table.reverse()
 symbol_table = [
     symbol_table[i]
     for i in range(len(symbol_table))
-    if symbol_table[i].id != None
+    if symbol_table[i].id != symbol_table[min(i + 1, len(symbol_table) - 1)].id
+    or i == len(symbol_table) - 1
 ]
 
+# Remove None from the table
 symbol_table = [
-    symbol_table[i]
-    for i in range(len(symbol_table))
-    if symbol_table[i].id != symbol_table[min(i+1, len(symbol_table)-1)].id
-    or
-    i == len(symbol_table)-1
+    symbol_table[i] for i in range(len(symbol_table)) if symbol_table[i].id != None
 ]
 
-print("ID\tData Type\tReturn Type\tInitial Value\tNo. of Parameters\tType of Paramameters", end="\n\n")
+print(
+    "ID\tData Type\tReturn Type\tInitial Value\tNo. of Parameters\tType of Paramameters",
+    end="\n\n",
+)
 for symbol in symbol_table:
     print(symbol)
